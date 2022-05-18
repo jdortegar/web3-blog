e95b9902f38884af6eade89b6ebfad9f336e93aa
David Ortega <ortega.david.ar@gmail.com>
2022-05-17 22:04:33 -0300

    post and edit post views

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  export const contractAddress = "0x10292CFcFb59B82d83480d0c5A8525b8Ee21DE4D"
+  export const ownerAddress = "0x0eb65D6c014fc987E33953aF803b5aEE7cA58C08"
   
